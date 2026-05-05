fe0c09448174c58601bc37b533e7be43b57105d0
cellopringga <celloondrea@gmail.com>
2026-05-05 14:44:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,33 @@
-const CACHE_NAME = 'cello-cv-v1';
+const CACHE_NAME = 'cello-pwa-v2';
 const assets = [
   '/',
   'index.html',
   'app.js',
   'cel.jpeg',
+  'jasa.jpeg',
+  'folio.jpeg',
+  'home.html',
   'about.html',
   'skills.html',
   'portfolio.html',
-  'experience.html'
+  'experience.html',
+  'certificate.html',
+  'contact.html',
+  'github.html'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
