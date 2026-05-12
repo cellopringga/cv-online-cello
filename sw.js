706e761eb5c32aebf26d16203eb5ef93a88c1050
cellopringga <celloondrea@gmail.com>
2026-05-12 13:20:25 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,47 @@
-const CACHE_NAME = 'cello-pwa-v2';
+const CACHE_NAME = 'cv-pwa-v3';
 const assets = [
-  '/',
-  'index.html',
-  'app.js',
-  'cel.jpeg',
-  'jasa.jpeg',
-  'folio.jpeg',
-  'home.html',
-  'about.html',
-  'skills.html',
-  'portfolio.html',
-  'experience.html',
-  'certificate.html',
-  'contact.html',
-  'github.html'
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './cel.jpeg',
+  './home.html',
+  './about.html',
+  './education.html',
+  './experience.html',
+  './skills.html',
+  './portfolio.html',
+  './certificate.html',
+  './contact.html'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
+// Install Service Worker
+self.addEventListener('install', evt => {
+  evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets');
       return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+// Activate dan Hapus Cache Lama
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetching
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
     })
   );
 });
